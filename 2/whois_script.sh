35d65d7b8f11a9103373d1235b7da529399fb951
Ilias <ildar97.97@gmail.com>
2018-10-09 03:37:10 +0300

    Fixed task 2

--- 2/whois_script.sh
+++ 2/whois_script.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 #==========================================================================
+curl --version
+if ! [[ $? -eq 0 ]]
+then
+    echo "Ошибка: пакет curl неустановлен"
+    exit 1
+fi
+echo
+
+
 # проверка числа параметров
 argc=3
 if (($# != $argc))
@@ -10,16 +19,13 @@ then
 fi
 
 
-# проверка существовании директории для сохранения файлов с результатами
+# попытка создания дирректории 
 results_dir=$1
-if ! [[ -d $results_dir ]]
+mkdir -p $results_dir
+# при отсутствии прав доступа скрипт завершит работу
+if ! [[ $? -eq 0 ]]
 then
-    echo "Ошибка: директория '$results_dir' не найдена"
-    exit 1
-# проверка возможности записи в эту дирректорию
-elif ! [[ -w $results_dir ]]
-then
-    echo "Ошибка: нет прав для создания файлов в директории '$results_dir'"
+    echo "Ошибка: директория '$results_dir' недоступна"
     exit 1
 fi
 
@@ -52,9 +58,18 @@ fi
 whois_service="https://www.whois.com/whois/"
 for domain in $domains_list
 do
-    echo "Запрос к $domain"
-    wget_fout=$(echo $domain | sed "s![/:\.]!_!g")".html"
-    wget --no-verbose -O $wget_fout "$whois_service""$domain"
+    # вообще говоря, существует еще много других протоколов кроме http и https
+    # то же верно и про сервисы, которые могли бы быть на месте www
+    # но для простоты команды учтем только их
+    purified_domain=$(echo $domain | sed -e '
+        s!^http://!!
+        s!^https://!!
+        s!^www\.!!
+        s!^ftp\.!!')
+    echo "Запрос к $purified_domain"
+    f_out=$(echo $purified_domain | sed "s!\.!_!g")".html"
+    #wget --no-verbose -O $wget_fout "$whois_service""$purified_domain"
+    curl -o $f_out -L -s "$whois_service""$purified_domain"
     echo
 done
 
